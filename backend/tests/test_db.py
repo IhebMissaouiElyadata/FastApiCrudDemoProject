0d9ffe1932906c0d3c939a5b8b679cc0988d6b69
Iheb Missaoui <iheb.missaoui@elyadata.com>
2024-02-19 14:56:22 +0100

    changing db to postgres and adapt test_db and envir variables to it

--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -1,15 +1,50 @@
-import mysql.connector
+# import mysql.connector
+#
+# def mysql_connection_tes():
+#     try:
+#
+#         conn = mysql.connector.connect(
+#             host="172.19.0.2", user="user", password="user", database="LLMBD"
+#         )
+#         conn.close()
+#         return True
+#     except Exception as e :
+#         print(f"Error connecting to MySQL database: {e}")
+#         return False
+# def test_mysql_connection():
+#     assert mysql_connection_tes() is True
 
-def mysql_connection_tes():
+import os
+import psycopg2
+from psycopg2 import OperationalError
+
+
+def test_connection():
     try:
+        # Connect to your PostgresSQL database
 
-        conn = mysql.connector.connect(
-            host="172.19.0.2", user="user", password="user", database="LLMBD"
+        conn = psycopg2.connect(
+            # i planned to use pytest in container (docker File) but despite i installed pytest using cmd in docker File , the command pytest was not recognized
+            # dbname=os.getenv("DB_NAME"),
+            # user=os.getenv("DB_USER"),
+            # password=os.getenv("DB_PASSWORD"),
+            # host=os.getenv("DB_HOST"),
+            # port=os.getenv("DB_PORT")
+            dbname="mydatabase",
+            user="user",
+            password="user",
+            host="172.24.0.2",
+            port=5432
         )
-        conn.close()
-        return True
-    except Exception as e :
-        print(f"Error connecting to MySQL database: {e}")
-        return False
-def test_mysql_connection():
-    assert mysql_connection_tes() is True
+        print("Connection successful!")
+        conn.close()  # Close the connection
+        assert True
+
+    except OperationalError as e:
+        print(f"Connection failed: {e}")
+        assert False
+
+
+# Test the connection
+def test_postgresql_connection():
+    test_connection()
